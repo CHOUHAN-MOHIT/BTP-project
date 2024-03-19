6926bf21df96a03308a40430e9a84d5a4b287f71
CHOUHAN-MOHIT <92783072+CHOUHAN-MOHIT@users.noreply.github.com>
2024-03-19 20:49:49 +0530

    implemented jwt auth for custom user.

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import Wedding
+from .models import Wedding , User
 
 class WeddingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wedding
-        #excluding invitation card (image data) for now
-        exclude = ('invitation_card',)
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id' , 'name' , 'email' , 'password']
+        extra_kwargs = {
+            'password': {'write_only':True}
+        }
+
+    def create(self , validate_data):
+        password = validate_data.pop('password' , None)
+        instance = self.Meta.model(**validate_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,17 +1,72 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import status
-from .models import Wedding
-from .serializers import WeddingSerializer
+from .serializers import UserSerializer  , WeddingSerializer
+from .models import User , Wedding
+import jwt , datetime
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome")
+class RegisterView(APIView):
+    def post(self , request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrrect Password')
+        
+        payload = {
+            'id' : user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload , 'secret' , algorithm='HS256')
+
+        response = Response()
+        response.set_cookie(key='jwt' , value=token, httponly=True)
+        response.data = {
+            'jwt' : token
+        }
+        return response
+
+class UserView(APIView):
+    def get(self , request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("UnAuthenticated")
+        
+        try:
+            payload = jwt.decode(token , 'secret' , algorithms='HS256')
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Invailid Token")
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
 
 
-
+class LogoutView(APIView):
+    def post(self , request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message':'success'
+        }
+        return response
 
 class WeddingListCreateView(APIView):
     def get(self, request):
@@ -22,6 +77,16 @@ class WeddingListCreateView(APIView):
 
     def post(self, request):
         """ Create a new wedding """
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed("UnAuthenticated")
+        
+        try:
+            payload = jwt.decode(token , 'secret' , algorithms='HS256')
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Invailid Token")
+        print(request.data)
         serializer = WeddingSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
