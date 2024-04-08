4aef2909023d7044f37228144d8ef40d4aeb9c25
CHOUHAN-MOHIT <92783072+CHOUHAN-MOHIT@users.noreply.github.com>
2024-04-09 00:14:21 +0530

    added field validation in login and register forms.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -10,62 +10,95 @@ const Login = ( { closeModal } ) => {
     const {auth ,setAuth} = UserDetails();
     const [email , setemail] = useState('');
     const [password , setpassword] = useState('');
-    const navigate= useNavigate();
-    const handleLogin = async (e) => {
-      e.preventDefault();
-  
-      try {
-          const response = await fetch('http://localhost:8000/apis/login', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              credentials: 'include',
-              body: JSON.stringify({
-                  email,
-                  password,
-              }),
-          });
-  
-          if (!response.ok) {
-              // Handle non-2xx response (e.g., 401 Unauthorized)
-              throw new Error(`Login failed with status: ${response.status}`);
-          }
-          // Assuming valid login
-          setAuth(true);
-          localStorage.setItem('isAuthenticated', true);
-  
-          // Call the closeModal function
-          closeModal();
-  
-      } catch (error) {
-          console.error('Error logging in:', error);
-      }
-  };
-  
-      
-    useEffect(()=>{
-        if(auth){
-            navigate("/weddings");
+    const [error , setError] = useState({
+        email:'',
+        password:'',
+        formError:''
+    });
+    useEffect(() => {
+        isValid()
+    } , [email , password]);
+    const isValid = () => {
+        let flag = true;
+        if (!email) {
+            setError(prevState => ({ ...prevState, email: 'Email is required.' }));
+            flag = false;
         }
-    },[auth]);
+        else{
+            setError(prevState => ({ ...prevState, email: '' }));
+        }
+        if (!password) {
+            setError(prevState => ({ ...prevState, password: 'Password is required.' }));
+            flag = false;
+        }
+        else{
+            setError(prevState => ({ ...prevState, password: '' }));
+        }
+        return flag;
+    }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if (!isValid()) {
+            return;
+        }
+    
+        try {
+            const response = await fetch('http://localhost:8000/apis/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            });
+    
+            const data = await response.json();
+    
+            if (!response.ok) {
+                // Update the error state with the server-side error message
+                setError({
+                    formError: data.error || 'An error occurred.',
+                    password: '',
+                    email:''
+                });
+                return;
+            }
+    
+            // Assuming valid login
+            setAuth(true);
+            localStorage.setItem('isAuthenticated', true);
+    
+            // Call the closeModal function
+            closeModal();
+    
+        } catch (error) {
+            console.error('Error logging in:', error);
+        }
+    };
+    
     return (
         <div className='pb-6 px-6'>
             <h3 className='text-center font-semibold text-lg mb-3'>Welcome Back</h3>
             <form onSubmit={handleLogin} className='flex flex-col'>
-                <input type="email" name='email' placeholder='john@mymail.com' className='form-control'
+                <input type="email" name='email' placeholder='john@mymail.com' className='form-control' 
                     onChange={e => setemail(e.target.value)}
                 />
-                <input type="password" name='password' placeholder='Password' className='form-control'
+                {error.email !== '' && <div className='mx-3 text-red-600 text-sm my-1'>* {error.email}</div>}
+                <input type="password" name='password' placeholder='Password' className='form-control' 
                     onChange={e => setpassword(e.target.value)}
                 />
-                <button className='border rounded-md p-2 mb-8 m-2 bg-highlight'>Login</button>
+                {error.password !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {error.password}</div>}
+                <button className='border rounded-md p-2 mt-6 mx-2 bg-highlight'>Login</button>
+                {error.formError !== '' && <div className='mx-3 text-red-600 text-sm my-1'>* {error.formError}</div>}
             </form>
-            <div className="separator">or Login with</div>
+            <div className="separator mt-6">or Login with</div>
             <div className='flex justify-around mt-8 m-4'>
-                <img src={facebookLogo} alt="" srcset="" />
-                <img src={googleLogo} alt="" srcset="" />
-                <img src={emailLogo} alt="" srcset="" />
+                <img src={facebookLogo} alt=""  />
+                <img src={googleLogo} alt=""  />
+                <img src={emailLogo} alt="" />
             </div>
        </div>
     )
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,23 +1,75 @@
-import React  , {useState} from 'react'
+import React  , {useState , useEffect } from 'react'
 import './Register.css'
 
 const Register = ( { closeModal } ) => {
-    const [name , setname] = useState('');
-    const [email , setemail] = useState('');
-    const [password , setpassword] = useState('');
+    const [formData , setFormData] = useState({
+        name:'',
+        email:'',
+        password:'',
+    })
+    const [error , setError] = useState({
+        name:'',
+        email:'',
+        password:'',
+        formError:'',
+    })
+    const [rePassword , setRePassword] = useState('');
+    const [rePasswordError , setRePasswordError] = useState('');
 
+    useEffect(() => {
+        isValid()
+    } , [formData , rePassword]);
+    const isValid = () => {
+        let flag = true;
+        if (!formData.name) {
+            setError(prevState => ({ ...prevState, name: 'Email is required.' }));
+            flag = false;
+        }
+        else{
+            setError(prevState => ({ ...prevState, name: '' }));
+        }
+        if (!formData.email) {
+            setError(prevState => ({ ...prevState, email: 'Email is required.' }));
+            flag = false;
+        }
+        else{
+            setError(prevState => ({ ...prevState, email: '' }));
+        }
+        if (!formData.password) {
+            setError(prevState => ({ ...prevState, password: 'Password is required.' }));
+            flag = false;
+        }
+        else{
+            setError(prevState => ({ ...prevState, password: '' }));
+        }
+        if (!rePassword) {
+            setRePasswordError('Password is required.' );
+            flag = false;
+        }
+        else{
+            setRePasswordError('');
+        }
+        if((formData.password && rePassword) && formData.password !== rePassword)
+        {
+            setRePasswordError('Password and confirm password do not match.' );
+            flag = false;
+        }
+        else if((formData.password && rePassword) && formData.password === rePassword)
+        {
+            setRePasswordError('' );
+        }
+        return flag;
+    }
     const handleRegister = async (e) => {
         e.preventDefault();
-    
+        if(!isValid()){
+            return;
+        }
         try {
             const response = await fetch('http://localhost:8000/apis/register', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    name,
-                    email,
-                    password
-                })
+                body: JSON.stringify(formData)
             });
     
             // Check if response is ok (HTTP status code in the range 200-299)
@@ -41,15 +93,24 @@ const Register = ( { closeModal } ) => {
             <h3 className='text-center font-semibold text-lg mb-3'>Lets Explore Indian Weddings!!</h3>
             <form className='flex flex-col' onSubmit={handleRegister}>
                 <input type="text" name='name' placeholder='Name' className='form-control'
-                    onChange={e => setname(e.target.value)}
+                    onChange={e => setFormData({...formData , name:e.target.value})}
                 />
+                {error.name !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {error.name}</div>}
                 <input type="email" name='email' placeholder='john@mymail.com' className='form-control'
-                    onChange={e => setemail(e.target.value)}
+                    onChange={e => setFormData({...formData , email:e.target.value})}
                 />
+                {error.email !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {error.email}</div>}
                 <input type="password" name='password' placeholder='Password' className='form-control'
-                    onChange={e => setpassword(e.target.value)}
+                    onChange={e => setFormData({...formData , password:e.target.value})}
                 />
-                <button className='border rounded-md p-2 m-2 bg-shade'>Register</button>
+                {error.password !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {error.password}</div>}
+                <input type="password" name='re-password' placeholder='Re-Password' className='form-control'
+                    onChange={e => setRePassword(e.target.value)}
+                />
+                {rePasswordError !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {rePasswordError}</div>}
+
+                <button className='border rounded-md p-2 mx-2 my-4 hover:bg-shade bg-orange-300'>Register</button>
+                {error.formError !== '' && <div className='mx-3 text-red-600 text-sm mt-1'>* {error.formError}</div>}
             </form>
             <div className="separator">OR</div>
             <div className='text-center mt-4'>Already a Member? Login</div>
