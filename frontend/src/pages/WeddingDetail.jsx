df5916d6987cdcfbae6b7b0ecfd834ab1a742319
CHOUHAN-MOHIT <92783072+CHOUHAN-MOHIT@users.noreply.github.com>
2024-05-18 23:13:39 +0530

    added back button in wedding detail page

--- frontend/src/pages/WeddingDetail.jsx
+++ frontend/src/pages/WeddingDetail.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import backIcon from '../assets/logos/icons8-back-arrow-50.png'
 const WeddingDetail = () => {
   const { id } = useParams();
   const [wedding, setWedding] = useState(null);
   const [events, setEvents] = useState([]);
   const [imageUrl, setImageUrl] = useState("http://127.0.0.1:8000");
+  const [guests, setGuests] = useState(1);
+  const pricePerGuest = 50; // Example price per guest
+  const serviceCharge = 20; // Example service charge
 
   const Event = ({ name, date, location, description }) => (
     <div className="border rounded-lg border-gray-300 p-4">
@@ -80,11 +83,11 @@ const WeddingDetail = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        credentials:"include",
+        credentials: "include",
         body: JSON.stringify({
           wedding_id: id,
-          amount: 50000 // Update this to the actual amount you want to charge
-        })
+          amount: totalAmount * 100, // Convert to smallest currency unit
+        }),
       });
 
       if (!response.ok) {
@@ -106,9 +109,6 @@ const WeddingDetail = () => {
           color: "#3399cc",
         },
         handler: function (response) {
-          // alert(response.razorpay_payment_id);
-          // alert(response.razorpay_order_id);
-          // alert(response.razorpay_signature);
           handlePaymentSuccess(response);
         },
         modal: {
@@ -127,21 +127,26 @@ const WeddingDetail = () => {
 
   const handlePaymentSuccess = async (response) => {
     try {
-      const successResponse = await fetch("http://localhost:8000/apis/payment_success/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}` // Adjust this line based on your auth setup
-        },
-        body: JSON.stringify({
-          razorpay_payment_id: response.razorpay_payment_id,
-          razorpay_order_id: response.razorpay_order_id,
-          razorpay_signature: response.razorpay_signature,
-        })
-      });
+      const successResponse = await fetch(
+        "http://localhost:8000/apis/payment_success/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`, // Adjust this line based on your auth setup
+          },
+          body: JSON.stringify({
+            razorpay_payment_id: response.razorpay_payment_id,
+            razorpay_order_id: response.razorpay_order_id,
+            razorpay_signature: response.razorpay_signature,
+          }),
+        }
+      );
 
       if (!successResponse.ok) {
-        throw new Error(`Error processing payment success: ${successResponse.statusText}`);
+        throw new Error(
+          `Error processing payment success: ${successResponse.statusText}`
+        );
       }
 
       alert("Payment Successful");
@@ -150,20 +155,31 @@ const WeddingDetail = () => {
     }
   };
 
+  const handleGuestChange = (e) => {
+    setGuests(e.target.value);
+  };
+  const incrementGuests = () => setGuests(guests + 1);
+  const decrementGuests = () => setGuests(guests > 1 ? guests - 1 : 1);
+
+  const totalAmount = pricePerGuest * guests + serviceCharge;
+
   return (
     <div className="relative m-16 px-4 py-8 flex justify-around">
       <div className="space-y-8 basis-1/2">
         {wedding ? (
           <>
-            <h1 className="text-3xl font-bold">
-              {wedding.bride_name} & {wedding.groom_name}'s Wedding{" "}
-            </h1>
+            <div className="flex flex-row items-center space-x-2">
+              <button onClick={() => window.history.back()} className="bg-base hover:bg-highlight rounded-full w-9 h-9"><img src={backIcon} alt="back icon" /></button>
+              <h1 className="text-3xl font-bold">
+                {wedding.bride_name} & {wedding.groom_name}'s Wedding{" "}
+              </h1>
+            </div>
             {wedding.invitation_card && (
               <div className="mt-8">
                 <img
                   src={imageUrl}
                   alt="Invitation Card"
-                  className="w-full rounded-lg shadow-lg mt-4"
+                  className="w-full rounded-lg shadow-lg mt-4 max-w-xl max-h-96 object-contain"
                 />
               </div>
             )}
@@ -206,18 +222,53 @@ const WeddingDetail = () => {
           <p className="text-xl">Loading wedding details...</p>
         )}
       </div>
-      <div className="space-y-8 basis-1/4">
-        <h1 className="text-3xl font-bold">Join Our Celebration of Love</h1>
-        <p className="text-lg">
+      <div className="space-y-4 basis-1/4">
+        <h1 className="text-2xl font-bold">Join Our Celebration of Love</h1>
+        <p className="">
           We are honored to invite you to join us in our joyous wedding
           celebrations. As a token of love and blessings, you may choose to
           contribute shagun to the bride and groom.
         </p>
+
+        <div className="border-t border-gray-200 pt-4">
+          <div className="flex justify-between text-sm">
+            <span>Price per Guest</span>
+            <span>${pricePerGuest}</span>
+          </div>
+          <div className="flex justify-between text-sm mt-2">
+            <span>Number of Guests</span>
+            <div className="text-lg font-semibold">
+              <div className="flex items-center">
+                <button
+                  onClick={decrementGuests}
+                  className="bg-gray-300 text-gray-800 px-4 py-1 rounded-l"
+                >
+                  -
+                </button>
+                <span className="px-4 py-2 bg-white text-sm">{guests}</span>
+                <button
+                  onClick={incrementGuests}
+                  className="bg-gray-300 text-gray-800 px-4 py-1 rounded-r"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between text-sm mt-2">
+            <span>Service Charge</span>
+            <span>${serviceCharge}</span>
+          </div>
+          <div className="flex justify-between text-sm font-semibold mt-4">
+            <span>Total Amount</span>
+            <span>${totalAmount}</span>
+          </div>
+        </div>
         <button
           onClick={handlePayment}
-          className="bg-white px-4 py-2 rounded border border-black"
+          className="mt-6 w-full px-4 py-2 bg-indigo-600 text-white font-semibold rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         >
-          Pay to Join
+          Pay ${totalAmount}
         </button>
       </div>
     </div>
