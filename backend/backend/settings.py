6efb17d323ef3eba8ee3e6111d3ce03304debc3f
CHOUHAN-MOHIT <92783072+CHOUHAN-MOHIT@users.noreply.github.com>
2024-05-18 09:15:43 +0530

    removed jwt middleware due to some issues

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'apis.middlewares.JWTMiddleware',
+    # 'apis.middlewares.JWTMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
